4f1b0ff8669ba5d242e83e0c52069bf8609bf297
Dhruw Vaviya <dhruwvaviya123@gmail.com>
2025-03-15 19:43:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 // Complete the js code
-function Car(make, model) {}
+class Car {
+	constructor(make, model) {
+		this.make = make,
+		this.model = model,
+	}
 
-function SportsCar(make, model, topSpeed) {}
+	getMakeModel(){
+		return `${make} ${model}`;	
+	}
+}
+
+class SportsCar extends Car {
+	constructor(make, model, topSpeed) {
+		this.topSpeed = topSpeed,
+	}
+
+	getTopSpeed(){
+		return topSpeed;
+	}
+}
+
+const car = new SportsCar("Ferrari", "Testarossa", 200);
+console.log(car.getMakeModel()); // Output: Ferrari Testarossa
+console.log(car.getTopSpeed()); // Output: 200
 
 // Do not change the code below
 window.Car = Car;
