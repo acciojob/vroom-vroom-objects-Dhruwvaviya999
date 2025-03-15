f819acbe02c1eabd46b35276f44240a37556448b
Dhruw Vaviya <dhruwvaviya123@gmail.com>
2025-03-15 19:47:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,38 @@
 // Complete the js code
-class Car {
-	constructor(make, model) {
-		this.make = make,
-		this.model = model,
-	}
-
-	getMakeModel(){
-		return `${make} ${model}`;	
-	}
+// Car Constructor
+function Car(make, model) {
+    this.make = make;
+    this.model = model;
 }
 
-class SportsCar extends Car {
-	constructor(make, model, topSpeed) {
-		this.topSpeed = topSpeed,
-	}
+// Add getMakeModel method to Car prototype
+Car.prototype.getMakeModel = function() {
+    return `${this.make} ${this.model}`;
+};
 
-	getTopSpeed(){
-		return topSpeed;
-	}
+// SportsCar Constructor (inherits from Car)
+function SportsCar(make, model, topSpeed) {
+    // Call Car constructor with .call() method to inherit properties
+    Car.call(this, make, model);
+    this.topSpeed = topSpeed;
 }
 
+// Inherit Car prototype methods
+SportsCar.prototype = Object.create(Car.prototype);
+
+// Set the constructor back to SportsCar
+SportsCar.prototype.constructor = SportsCar;
+
+// Add getTopSpeed method to SportsCar prototype
+SportsCar.prototype.getTopSpeed = function() {
+    return this.topSpeed;
+};
+
+// Test the solution
 const car = new SportsCar("Ferrari", "Testarossa", 200);
 console.log(car.getMakeModel()); // Output: Ferrari Testarossa
-console.log(car.getTopSpeed()); // Output: 200
+console.log(car.getTopSpeed());   // Output: 200
+
 
 // Do not change the code below
 window.Car = Car;
